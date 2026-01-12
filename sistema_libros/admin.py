b8998bd986047a7ef950660455c7bf1cb163ee62
Pal-cloud <paloma.gom.sal@gmail.com>
2026-01-12 10:33:51 +0100

    add admin and views

--- sistema_libros/admin.py
+++ sistema_libros/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Libro
 
-# Register your models here.
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'fecha_publicacion', 'isbn')
+    search_fields = ('titulo', 'autor', 'isbn')
+    list_filter = ('fecha_publicacion',)
