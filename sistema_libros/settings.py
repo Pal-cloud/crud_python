5520d4bd5fae508d547d3a2879a9aebe90c394f0
Pal-cloud <paloma.gom.sal@gmail.com>
2026-01-12 10:29:46 +0100

    migrations

--- sistema_libros/settings.py
+++ sistema_libros/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sistema_libros',
+    'categorias',
+    'libreria',
 ]
 
 MIDDLEWARE = [
